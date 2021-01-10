84c4c1c6f25fa28572fa7de78338ab4e746ddce1
Lagar <lucasayalaeducacion@gmail.com>
2021-01-10 02:04:04 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,19 @@
 // PUERTO:
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ENTORNO:
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// BASE DE DATOS:
+let urlDb;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDb = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDb = 'mongodb+srv://proyect0:VCP7qw3DMBBh8DsH@cluster0.o5aln.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDb;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require('./config/config');
 
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express()
 
 const bodyParser = require('body-parser')
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -12,32 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
- 
-app.get('/usuario', function (req, res) {
-  res.json('Get usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
+mongoose.connect(process.env.URLDB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, (err, resp) => {
 
-    let body = req.body;
+  if(err) throw err;
 
-    res.json({
-        persona: body
-    })
-  });
-
-app.put('/usuario/:id', function (req, res) {
-    
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('Delete usuario')
-  });
+  console.log('Base de datos ONLINE')
+},);
 
 
 app.listen(process.env.PORT, () =>{
